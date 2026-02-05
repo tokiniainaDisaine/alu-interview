fb02a5e2c3cc7a8cab9be8260bfd7868dea9460a
TheRealToky <tokydisaine.andrianarison@gmail.com>
2026-02-05 21:46:28 +0200

    improve algorithm

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,35 +3,21 @@
 Docstring for rain.0-rain
 """
 
+
 def rain(walls):
     """
     Docstring for rain
-    
+
     :param walls: Description
     """
-    num_of_walls = []
-    num_of_zeros = []
+    if not walls or len(walls) < 3:
+        return 0
 
-    total_area = 0
-    zero_counter = 0
-
-    for wall_height in walls:
-        if wall_height != 0:
-            num_of_walls.append(wall_height)
-            num_of_zeros.append(zero_counter)
-            zero_counter = 0
-        else:
-            zero_counter += 1
-
-    num_of_zeros.pop(0)
-
-    for i in range(len(num_of_walls) - 1):
-        j = min(i, (i + 1)) # the highest water elevation
-        height = num_of_walls[j]
-        length = num_of_zeros[i]
-
-        water_area = height * length
-        total_area += water_area
-
-    return total_area
-    
+    rain = 0
+    for i in range(1, len(walls) - 1):
+        left = max(walls[:i])
+        right = max(walls[i + 1:])
+        min_wall = min(left, right)
+        if walls[i] < min_wall:
+            rain += min_wall - walls[i]
+    return rain
